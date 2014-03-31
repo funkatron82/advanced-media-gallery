35a247cc171f0a06969baf37d8269e6547efdbdc
Manny Fleurmond <funkatronic@gmail.com>
2014-03-31 09:20:15 -0500

    Using plugins_loaded (thanks to Franz Kaiser)

--- advanced-media-galleries.php
+++ advanced-media-galleries.php
@@ -9,19 +9,22 @@ Author URI: http://www.crosseyedesign.com
 License: GPL2
 */
 
-
-//Config
-require_once( 'config.php' );
-
-//Core
-require_once( AMG_INC_DIR . 'core/core.php' );
-
-//Admin
-if( is_admin() ){
-	require_once( AMG_INC_DIR . 'admin/admin.php' );
+function amg_plugin_load() {
+	//Config
+	require_once( 'config.php' );
+	
+	//Core
+	require_once( AMG_INC_DIR . 'core/core.php' );
+	
+	//Admin
+	if( is_admin() ){
+		require_once( AMG_INC_DIR . 'admin/admin.php' );
+	}
+	
+	//Front end
+	else{
+		//require_once( AMG_INC_DIR . 'core/front-end.php'  );
+	}
 }
 
-//Front end
-else{
-	//require_once( AMG_INC_DIR . 'core/front-end.php'  );
-}
+add_action( 'plugins_loaded', 'amg_plugin_load' );
